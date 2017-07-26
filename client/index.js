3723ee4ead2f347ad65f9933e343fc8d29f3ee11
Michael North <michael.l.north@gmail.com>
2017-07-26 19:38:33 +0000

    apple home screen icons

--- client/index.js
+++ client/index.js
@@ -7,5 +7,14 @@ import App from './app';
 import 'file-loader?name=./img/launcher-icon-1x.png!./img/launcher-icon-1x.png';
 import 'file-loader?name=./img/launcher-icon-2x.png!./img/launcher-icon-2x.png';
 import 'file-loader?name=./img/launcher-icon-4x.png!./img/launcher-icon-4x.png';
+import 'file-loader?name=./apple-touch-icon-57x57.png!./img/apple-touch-icon-57x57.png';
+import 'file-loader?name=./apple-touch-icon-60x60.png!./img/apple-touch-icon-60x60.png';
+import 'file-loader?name=./apple-touch-icon-72x72.png!./img/apple-touch-icon-72x72.png';
+import 'file-loader?name=./apple-touch-icon-76x76.png!./img/apple-touch-icon-76x76.png';
+import 'file-loader?name=./apple-touch-icon-114x114.png!./img/apple-touch-icon-114x114.png';
+import 'file-loader?name=./apple-touch-icon-120x120.png!./img/apple-touch-icon-120x120.png';
+import 'file-loader?name=./apple-touch-icon-144x144.png!./img/apple-touch-icon-144x144.png';
+import 'file-loader?name=./apple-touch-icon-152x152.png!./img/apple-touch-icon-152x152.png';
+import 'file-loader?name=./apple-touch-icon-180x180.png!./img/apple-touch-icon-180x180.png';
 
 ReactDOM.render((<App />), document.getElementById('root'));
