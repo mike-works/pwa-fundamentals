b4a5d8b2e6b61c455c1c838cac6792745ee8ef24
Michael North <michael.l.north@gmail.com>
2017-07-27 21:15:30 -0500

    service worker cleanup

--- client/sw/caches.js
+++ client/sw/caches.js
@@ -1,112 +1,36 @@
-/* eslint no-console: 0*/
-const CACHE_VERSION = 25;
-const CACHE_KEY = `FEG-v${CACHE_VERSION}`;
-const CACHE_NAME = (name) => `${CACHE_KEY}-${name}`;
+const CACHE_VERSION = 1;
+const CACHE_PREFIX = `FEG-v${CACHE_VERSION}`;
 
 const ALL_CACHES = {
-  prefetch: CACHE_NAME('PREFETCH'),
-  fallback: CACHE_NAME('FALLBACK')
+  fallbackImages: cacheName('FALLBACK_IMAGES'),
+  prefetch: cacheName('PREFETCH'),
+  fallback: cacheName('FALLBACK')
 };
 
-const ALL_CACHES_LIST = Object.keys(ALL_CACHES).map((k) => ALL_CACHES[k]);
-
-const ASSET_MANIFEST_URL = `${self.location.protocol}//${self.location.host}/asset-manifest.json`;
-
-/// PREFETCH CACHE ///
-const PREFETCH_WHITELIST = ['app.js', 'web-app-manifest.json', /^img\/[\w0-9\-_]+.(png|jpg|gif|bmp)$/];
-const PREFETCH_BLACKLIST = [];
-
-function isRegex(x) {
-  return typeof x === 'object' && !!x.test && !!x.exec;
+function cacheName(name) {
+  return `${CACHE_PREFIX}-${name}`;
 }
 
-function doesFilenameMatchPrefetchFilter(fileName, filter) {
-  if (typeof filter === 'string') {
-    return filter.toLowerCase() === fileName.toLowerCase();
-  } else if (isRegex(filter)) {
-    return filter.test(fileName);
-  } else {
-    console.error('SW: Unexpected type in prefetch filter list!', filter);
-  }
-}
+export const ALL_CACHES_LIST = Object
+  .keys(ALL_CACHES)
+  .map((k) => ALL_CACHES[k]);
 
-export function handleIfPrecached(request) {
-  return self.caches.match(request, { cacheName: ALL_CACHES.prefetch }).then((r) => {
-    if (!r){
-      throw `Did not match anything in precache ${ request.url}`;
-    }
-    return r;
-  })
-}
-
-export function fetchWithCacheFallback(request, timeout) {
-  return new Promise((resolve, reject) => {
-    let t = setTimeout(reject, timeout);
-    fetch(request).then((response) => {
-      clearTimeout(t);
-      caches.open(ALL_CACHES.fallback).then((cache) => {
-        console.log('adding to cache ', request.url);
-        cache.add(request.url, response);
-        resolve(response);
-      });
-    }, reject) 
-  }).catch(() => {
-    return caches.open(ALL_CACHES.fallback).then((cache) => {
-      return cache.match(request).then((match) => {
-        return match || fetch(request);
-      }).catch(() => {
-        return fetch(request);
-      })
-    })
-  });
-}
-
-function _shouldPrefetchFile(fileName) {
-  for (let i = 0; i < PREFETCH_BLACKLIST.length; i++) {
-    if (doesFilenameMatchPrefetchFilter(fileName, PREFETCH_BLACKLIST[i])) {
-      return false;
-    }
-  }
-  for (let i = 0; i < PREFETCH_WHITELIST.length; i++) {
-    if (doesFilenameMatchPrefetchFilter(fileName, PREFETCH_WHITELIST[i])) {
-      return true;
-    }
-  }
-  return false;
-}
-
-export function prefetchStaticAssets() {
-  return caches.open(ALL_CACHES.prefetch)
-    .then((cache) => {
-      console.info('Retrieving asset manifest: ', ASSET_MANIFEST_URL);
-      return fetch(ASSET_MANIFEST_URL)
-        .then(response => response.json())
-        .then((m) => {
-          console.log('Asset Manfest is: ', m);
-          let toCache = Object.keys(m)
-            .filter(_shouldPrefetchFile)
-            .map((k) => m[k]);
-          toCache.push('/');
-          return cache.addAll(toCache).then((x) => {
-            console.log('Precache complete');
-            return x;
-          });
-        });
-    });
-}
-
-export function cleanUnusedCaches() {
+/**
+ * Delete all caches other than those whose names are
+ * provided in a list
+ * 
+ * @public
+ * @param {string[]} cacheNamesToKeep names of caches to keep
+ * @return {Promise}
+ */
+export function removeUnusedCaches(cacheNamesToKeep) {
   return caches.keys().then((cacheNames) => {
-    let toDelete = cacheNames
-      .reduce((list, thisCache) => {
-        if (ALL_CACHES_LIST.indexOf(thisCache) === -1) {
-          return list.concat(thisCache);
-        } else {
-          return list;
-        }
-      }, []);
+    let toDelete = cacheNames.reduce((list, thisCache) => {
+      if (cacheNamesToKeep.indexOf(thisCache) === -1)
+        return list.concat(thisCache);
+      return list;
+    }, []);
     if (toDelete.length > 0) {
-      // eslint-disable-next-line
       console.log('SW: Deleting old caches', toDelete);
       return Promise.all(toDelete.map((c) => caches.delete(c)));
     } else {
@@ -114,3 +38,27 @@ export function cleanUnusedCaches() {
     }
   });
 }
+
+////////////////////
+// PREFETCH CACHE //
+////////////////////
+// const ASSET_MANIFEST_URL = `${self.location.protocol}//${self.location.host}/asset-manifest.json`;
+// const RESOURCES_TO_PRECACHE = [
+//   /^app.js$/,
+//   /^web-app-manifest.json$/,
+//   /^img\/[\w0-9\-_]+.(png|jpg|gif|bmp)$/
+// ];
+
+/**
+ * Check whether a given filename represents a resource
+ * that should be precached
+ * 
+ * @private
+ * @param {string} fileName 
+ * @return {boolean}
+ */
+// function _shouldPrecacheFile(fileName) {
+//   for (let i = 0; i < RESOURCES_TO_PRECACHE.length; i++)
+//     if (RESOURCES_TO_PRECACHE[i].test(fileName)) return true;
+//   return false;
+// }
