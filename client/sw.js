ff3916d02a7d5286734c0d5544d8f37986166fdb
Michael North <michael.l.north@gmail.com>
2017-07-28 13:40:09 -0500

    starting point for caching JSON

--- client/sw.js
+++ client/sw.js
@@ -39,19 +39,37 @@ function fetchImageOrFallback(fetchEvent) {
   })
 }
 
+/**
+ * @return {Promise<Response>}
+ */
+function fetchApiJsonWithFallback() {
+  return caches.open(ALL_CACHES.fallback).then((/* cache */) => {
+    // cache.add or addAll (request or url)
+  })
+
+  // try to go to the network for some json
+  //   when it comes back, begin the process of putting it in the cache
+  //   and resolve the promise with the original response 
+  // in the event that it doesn't work out
+  // serve from the cache
+}
+
 self.addEventListener('fetch', event => {
   let acceptHeader = event.request.headers.get('accept');
   let requestUrl = new URL(event.request.url);
+
+  let isGroceryImage = acceptHeader.indexOf('image/*') >= 0 && requestUrl.pathname.indexOf('/images/') === 0;
+  let isApiJson = false;
 
   event.respondWith(
     caches.match(event.request, {cacheName: ALL_CACHES.prefetch}).then((response) => {
       // If a precached thing is found, go with it
       if (response) return response;
       // Otherwise, let's dig deeper
-      if (acceptHeader.indexOf('image/*') >= 0) {
-        if (requestUrl.pathname.indexOf('/images/') === 0) {
-          return fetchImageOrFallback(event);
-        }
+      if (isGroceryImage) {
+        return fetchImageOrFallback(event);
+      } else if (isApiJson) {
+        return fetchApiJsonWithFallback(event);
       }
       return fetch(event.request);
     })
