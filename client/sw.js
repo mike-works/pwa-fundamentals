09f4d902b3e1e747c1293d2eac2924cd5308e896
Michael North <michael.l.north@gmail.com>
2017-07-28 10:54:37 -0500

    Image fallback

--- client/sw.js
+++ client/sw.js
@@ -1,21 +1,44 @@
-const counts = {
-  installs: 0,
-  activations: 0,
-  fetches: 0
-};
+const FALLBACK_IMAGE_URL = 'https://localhost:3100/images/fallback-grocery.png';
 
-self.helloWorld = function helloWorld() {
-  console.log('Hello world!!!!!');
-}
+const fallbackImages = 'fallback-images';
 
-self.addEventListener('install', () => {
-  console.log('install',++counts.installs);
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(fallbackImages)
+      .then(cache => {
+        cache.add(FALLBACK_IMAGE_URL);
+      })
+  )
 });
 
 self.addEventListener('activate', () => {
-  console.log('activate', ++counts.activations);
+  caches.keys().then(cacheNames => {})
 });
 
-self.addEventListener('fetch', () => {
-  console.log('fetch', ++counts.fetches);
+function fetchImageOrFallback(fetchEvent) {
+  return fetch(fetchEvent.request, { 
+    mode: 'cors'
+  }).then(response => {
+    if (!response.ok) {
+      return caches.match(FALLBACK_IMAGE_URL, { cacheName: fallbackImages });
+    } else {
+      return response;
+    }
+  }).catch(error => {
+    console.error(error);
+    return caches.match(FALLBACK_IMAGE_URL, { cacheName: fallbackImages });
+  })
+}
+
+self.addEventListener('fetch', event => {
+  let acceptHeader = event.request.headers.get('accept');
+  let requestUrl = new URL(event.request.url);
+
+  if (acceptHeader.indexOf('image/*') >= 0) {
+    if (requestUrl.pathname.indexOf('/images/') === 0) {
+      event.respondWith(
+        fetchImageOrFallback(event)
+      )
+    }
+  }
 });
