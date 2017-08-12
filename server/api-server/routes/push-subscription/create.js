8f75ade51e580574da123d1c1caad3dad9fa91fe
Michael North <michael.l.north@gmail.com>
2017-08-12 16:10:35 -0700

    Only allow one push subscription to exist at any time

--- server/api-server/routes/push-subscription/create.js
+++ server/api-server/routes/push-subscription/create.js
@@ -3,12 +3,16 @@ module.exports = function (api) {
 
   return function (req, res) {
     let { endpoint, keys } = req.body;
-    PushSubscription.create({endpoint, keys: JSON.stringify(keys)}).then((ps) => {
-      let record = ps.get({plain: true});
-      record.keys = JSON.parse(record.keys);      
-      res.json(record);
-    }).catch((err) => {
-      res.json({ error: `Problem saving data: ${err}` });
+    PushSubscription.destroy({
+      truncate: true
+    }).then(() => {
+      return PushSubscription.create({endpoint, keys: JSON.stringify(keys)}).then((ps) => {
+        let record = ps.get({plain: true});
+        record.keys = JSON.parse(record.keys);      
+        res.json(record);
+      }).catch((err) => {
+        res.json({ error: `Problem saving data: ${err}` });
+      });
     });
   }
 }
