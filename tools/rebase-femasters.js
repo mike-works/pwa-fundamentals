2dfe0cc41183994a175e159adad82d3818077785
Michael North <michael.l.north@gmail.com>
2017-08-12 15:08:55 -0700

    Frontend Masters rebase script

--- tools/rebase-femasters.js
+++ tools/rebase-femasters.js
@@ -3,20 +3,26 @@
 const { spawn } = require('child_process');
 
 function checkoutBranch(branchName) {
+  console.log('scheduled CHECKOUT ', branchName);
   return new Promise((resolve, reject) => {
+    console.log('CHECKOUT ', branchName);
     let gco = spawn('git', ['checkout', branchName]);
     gco.stdout.pipe(process.stdout);
     gco.on('close', () => {
+      console.log('complete CHECKOUT ', branchName)
       resolve();
     });
   });
 }
 
 function rebaseOnto(baseBranchName) {
+  console.log('scheduled REBASE ', baseBranchName);
   return new Promise((resolve, reject) => {
+    console.log('REBASE ', baseBranchName);
     let gco = spawn('git', ['rebase', baseBranchName]);
     gco.stdout.pipe(process.stdout);
     gco.on('close', () => {
+      console.log('complete REBASE ', baseBranchName)
       resolve();
     });
   });
@@ -34,7 +40,7 @@ let branches = [
   'femasters/1-begin', 'femasters/1-complete',
   'femasters/2-begin', 'femasters/2-complete',
   'femasters/3-begin', 'femasters/3-complete',
-  'femasters/4-begin', 'femasters/4-complete',
+  'femasters/4-begin', 'femasters/4-complete'/*,
   'femasters/5-begin', 'femasters/5-complete',
   'femasters/6-begin', 'femasters/6-complete',
   'femasters/7-begin', 'femasters/7-complete',
@@ -44,7 +50,7 @@ let branches = [
   'femasters/11-begin', 'femasters/11-complete',
   'femasters/12-begin', 'femasters/12-complete',
   'femasters/13-begin', 'femasters/13-complete',
-  'femasters/final'
+  'femasters/final'*/
 ];
 let p = Promise.resolve();
 for (let i = 1; i < branches.length; i++) {
