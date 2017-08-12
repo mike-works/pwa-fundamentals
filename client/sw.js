dc4f919031d8e5ceceb46c853d82e8154c23288f
Michael North <michael.l.north@gmail.com>
2017-08-12 09:29:27 -0700

    ✅ Exercise 7 - Intercepting fetch - fallback grocery images

--- client/sw.js
+++ client/sw.js
@@ -1,15 +1,38 @@
-let counts = {
-  install: 0,
-  activate: 0,
-  fetch: 0
-};
+const FALLBACK_IMAGE_URL = 'https://localhost:3100/images/fallback-grocery.png';
 
-self.addEventListener('install', () => {
-  console.log('install: ', counts.install++);
+const FALLBACK_IMAGES = 'fallback-images';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(FALLBACK_IMAGES).then(cache => {
+      return cache.add(FALLBACK_IMAGE_URL)
+    })
+  );
 });
+
 self.addEventListener('activate', () => {
-  console.log('activate: ', counts.activate++);
+
 });
-self.addEventListener('fetch', () => {
-  console.log('fetch: ', counts.fetch++);
+
+function fetchImageOrFallback(fetchEvent) {
+  return fetch(fetchEvent.request, {mode: 'cors'})
+    .then((response) => {
+      if (!response.ok){
+        return caches.match(FALLBACK_IMAGE_URL, { cacheName: FALLBACK_IMAGES});
+      }
+      return response;
+    })
+    .catch(() => caches.match(FALLBACK_IMAGE_URL, { cacheName: FALLBACK_IMAGES}));
+}
+
+self.addEventListener('fetch', event => {
+  let acceptHeader = event.request.headers.get('accept');
+  let requestUrl = new URL(event.request.url);
+  let isGroceryImage = acceptHeader.indexOf('image/*') >= 0 && requestUrl.pathname.indexOf('/images/') === 0;
+
+  if (acceptHeader && isGroceryImage) {
+    event.respondWith(
+      fetchImageOrFallback(event)
+    );
+  }
 });
