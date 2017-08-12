39bb44af676cc98abbf7ffaa4d86f7a875eb51c1
Michael North <michael.l.north@gmail.com>
2017-08-12 10:25:37 -0700

    🚀 Exercise 9 - Caching JSON in a service worker (starting point)

--- client/sw.js
+++ client/sw.js
@@ -33,11 +33,26 @@ function fetchImageOrFallback(fetchEvent) {
     .catch(() => caches.match(FALLBACK_IMAGE_URL, { cacheName: FALLBACK_IMAGES}));
 }
 
+/**
+ * @return {Promise<Response>}
+ */
+function fetchApiJsonWithFallback(/* fetchEvent */) {
+  return caches.open(ALL_CACHES.fallback).then((/* cache */) => {
+    // cache.add or addAll (request or url)
+  })
+
+  // try to go to the network for some json
+  //   when it comes back, begin the process of putting it in the cache
+  //   and resolve the promise with the original response 
+  // in the event that it doesn't work out
+  // serve from the cache
+}
+
 self.addEventListener('fetch', event => {
   let acceptHeader = event.request.headers.get('accept');
   let requestUrl = new URL(event.request.url);
   let isGroceryImage = acceptHeader.indexOf('image/*') >= 0 && requestUrl.pathname.indexOf('/images/') === 0;
-
+  let isApiJson = false;
   event.respondWith(
     caches.match(event.request, { cacheName: ALL_CACHES.prefetch })
       .then(response => {
@@ -46,6 +61,8 @@ self.addEventListener('fetch', event => {
         // Handle grocery images
         if (acceptHeader && isGroceryImage) {
           return fetchImageOrFallback(event)
+        } else if (isApiJson) {
+          return  fetchApiJsonWithFallback(event)
         } else {
           // Everything else falls back to the network
           return fetch(event.request);
