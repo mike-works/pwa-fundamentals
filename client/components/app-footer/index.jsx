5d4d3e5e532cf0343764a55960e3a2b96f1dec93
Michael North <michael.l.north@gmail.com>
2017-08-15 10:07:59 -0700

    Change footer

--- client/components/app-footer/index.jsx
+++ client/components/app-footer/index.jsx
@@ -5,7 +5,7 @@ import './styles.scss';
 const AppFooter = () => (
   <footer className="footer">
     <div className="mui-container-fluid">
-      🙌 I built this in a <a href="https://frontendmasters.com" rel="noopener noreferrer" target="_blank">Frontend Masters</a> PWA course.
+      🙌 I built this in a <a href="https://mike.works/course/progressive-web-fundamentals-0d74af5" rel="noopener noreferrer" target="_blank">Mike Works</a> PWA course.
     </div>
   </footer>
 );
