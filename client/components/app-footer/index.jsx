47b31c3314acbfe200a3dfe6ea31b6917541f065
Michael North <michael.l.north@gmail.com>
2017-08-15 11:06:48 -0700

    resolve conflicts

--- client/components/app-footer/index.jsx
+++ client/components/app-footer/index.jsx
@@ -5,11 +5,7 @@ import './styles.scss';
 const AppFooter = () => (
   <footer className="footer">
     <div className="mui-container-fluid">
-<<<<<<< HEAD
       🙌 I built this in a <a href="https://go/training-schedule" rel="noopener noreferrer" target="_blank">LinkedIn</a> PWA course.
-=======
-      🙌 I built this in a <a href="https://mike.works/course/progressive-web-fundamentals-0d74af5" rel="noopener noreferrer" target="_blank">Mike Works</a> PWA course.
->>>>>>> master
     </div>
   </footer>
 );
