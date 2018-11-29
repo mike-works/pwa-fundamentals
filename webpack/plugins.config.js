c400b3c65e229b07c4ec372cb81b30a96a0a90f6
Jozef Piecyk <jozollo@gmail.com>
2018-11-29 19:54:03 +0100

    feat(webpack): remove UglifyJsPlugin

--- webpack/plugins.config.js
+++ webpack/plugins.config.js
@@ -30,23 +30,6 @@ module.exports = function(env, options) {
       })
     );
     plugins.push(
-      new webpack.optimize.UglifyJsPlugin({
-        sourceMap:
-          options.devtool &&
-          (options.devtool.indexOf('sourcemap') >= 0 ||
-            options.devtool.indexOf('source-map') >= 0),
-        beautify: false,
-        mangle: {
-          screw_ie8: true,
-          keep_fnames: true
-        },
-        compress: {
-          screw_ie8: true
-        },
-        comments: false
-      })
-    );
-    plugins.push(
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production')
       })
